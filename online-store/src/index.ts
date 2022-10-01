68f5ce1cbdca759d2c43a3bcd1b9c00a3def6e27
atmoranso <atmoranso@gmail.com>
2022-10-01 15:11:24 +0300

    fix: delete logs

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,9 +1,5 @@
 import 'nouislider/dist/nouislider.css';
 import './sass/style.scss';
 import App from './scripts/app';
-console.log(
-    `Добрый день. Спасибо, что зашли :). Задание полностью выполнено, все пункты по ТЗ сделаны и работают.\n\nОбщая оценка 220/220\n\nУдачи в учебе! :)`
-);
-
 const app = new App();
 app.start();
